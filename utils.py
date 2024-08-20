3915d3845a89d7c5fc0e82c8a52fba9cb2af094c
nbrc <47108281+nbrochec@users.noreply.github.com>
2024-08-20 18:53:48 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -13,31 +13,49 @@
 from os.path import join, dirname, basename, abspath, normpath, isdir
 from os import listdir
 from glob import glob
+import torchaudio.transforms as Taudio
+import torch.nn.functional as Fnn
+import torch
 
-def browse_subfolders(folder):
-    subfolder_list = []
-    
-    for folder_name in listdir(folder):
-        path = join(folder, folder_name)
-        if isdir(path):
-            subfolder_list.append(folder_name)
-            sous_dossiers = browse_subfolders(path) 
-            subfolder_list.extend(sous_dossiers)
+class customLogMelSpectrogram():
+    def __init__(self, sample_rate=24000, n_fft=2048, win_length=None, hop_length=512, n_mels=128, f_min=150, f_max=None):
+        self.sample_rate = sample_rate
+        self.n_fft = n_fft
+        self.win_length = win_length if win_length is not None else n_fft
+        self.hop_length = hop_length
+        self.n_mels = n_mels
+        self.f_min = f_min
+        self.f_max = f_max if f_max is not None else sample_rate / 2
         
-    subfolder_list.sort()
-    return subfolder_list
+        self.mel_scale = Taudio.MelSpectrogram(
+            sample_rate=self.sample_rate,
+            n_fft=self.n_fft,
+            win_length=self.win_length,
+            hop_length=self.hop_length,
+            n_mels=self.n_mels,
+            f_min=self.f_min,
+            f_max=self.f_max,
+            power=2.0
+        )
 
-def get_sound_files(datafolderpath):
-    database_path = dirname(datafolderpath+'/')
-
-    all_path = []
-    extensions = ['*.wav', '*.mp3', '*.aiff']
-    for ext in extensions:
-        all_path.extend(glob(join(database_path+'/*/', ext)))
-
-    all_names = []
-    for path in range(len(all_path)):
-        all_names.append(basename(all_path[path]))
+        self.amplitude_to_db = Taudio.AmplitudeToDB(stype='power', top_db=80.0)
     
-    print(f'{len(all_path)} files found')
-    return all_path, all_names
+    def __call__(self, waveform):
+        mel_spec = self.mel_scale(waveform)
+        mel_spec_db = self.amplitude_to_db(mel_spec)
+        mel_spec_db = Fnn.normalize(mel_spec_db, dim=2)
+        mel_spec_db = Fnn.normalize(mel_spec_db, dim=3)
+        return mel_spec_db
+    
+    def process_segment(self, waveform, segment_length=7680):
+        """Divides the waveform into segments of a fixed length and applies the Mel transformation."""
+        segments = []
+        for start in range(0, waveform.size(1) - segment_length + 1, segment_length):
+            segment = waveform[:, start:start + segment_length]
+            mel_spec_db = self.__call__(segment)
+            segments.append(mel_spec_db)
+        return torch.stack(segments, dim=0)  # (num_segments, 1, 128, 15)
+
+
+
+
