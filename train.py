3b78ecabf3f754cb8031172d1b1ef1e87c41e2d0
nbrc <47108281+nbrochec@users.noreply.github.com>
2024-08-20 18:56:32 +0900

    Update train.py

--- train.py
+++ train.py
@@ -12,46 +12,34 @@
 
 import argparse, sys, math, os
 from architectures import v1, v2, one_residual, two_residual, transformer
-import torch
+import torch, torchaudio
 from glob import glob
 from os.path import join
 
 parser = argparse.ArgumentParser(description='train CNN model for RT-IPT-R')
-parser.add_argument('--train_dir', type=str, required=True, help='train samples directory')
-parser.add_argument('--test_dir', type=str, required=True, help='test samples directory')
+parser.add_argument('--datasets_dir', type=str, required=True, help='dataset directory where h5 files are saved')
 parser.add_argument('--epochs', type=int, help='number of train epochs', default=100)
 parser.add_argument('--config', type=str, help='version of the CNN', default='v2')
 parser.add_argument('--device', type=int, help='gpu device', default='cuda')
 parser.add_argument('--gpu', type=int, help='gpu device number', default=0)
 parser.add_argument('--log_dir', type=int, help='log directory', default='./log_dir')
-parser.add_argument('--sr', type=int, help='sampling rate', default=24000)
 parser.add_argument('--augment', type=str, help='augmentations', default='pitchshift')
 
 args = parser.parse_args()
 
-if not args.train_dir:
-    print('Error: train samples directory is mandatory.')
+if not args.datasets_dir:
+    print('Error: datasets directory is required to launch training.')
+    print('Please indicate the path of the datasets directory.')
     parser.print_help()
     sys.exit(1)
 
-if not args.test_dir:
-    print('Error: test samples directory is mandatory.')
-    parser.print_help()
-    sys.exit(1)
-
-train_dir = args.train_dir
-test_dir = args.tset_dir
+datasets_dir = args.train_dir
 epochs = args.epochs
 config = args.config
 device = args.device
 gpu = args.gpu
 log_dir = args.log_dir
-sr = args.sr
 augment = args.augment
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f'Current device: {device}')
-
-# Load Train Dataset
-print('Load Train Dataset')
-
