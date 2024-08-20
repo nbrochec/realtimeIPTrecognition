0c353da49aa42fc633729e43b9e1506de385db22
nbrc <47108281+nbrochec@users.noreply.github.com>
2024-08-20 19:24:39 +0900

    Update license text

--- augmentations.py
+++ augmentations.py
@@ -4,7 +4,7 @@
 # TOKYO UNIVERSITY OF THE ARTS
 # 東京藝術大学音楽音響創造科
 # ERC Reach
-# MIT license 2024
+# GNU General Public License v3.0
 #############################################################################
 # Code description:
 # Implement data augmentation methods
