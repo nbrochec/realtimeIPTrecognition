dd5fcf3013caa7d0fd8f4afe00cd51aafb92d4ac
nbrc <47108281+nbrochec@users.noreply.github.com>
2024-08-21 16:36:59 +0900

    Update augmentations.py
    Implement the ApplyAugmentation class structure.

--- augmentations.py
+++ augmentations.py
@@ -19,3 +19,91 @@ Principally using torch_audiomentations because:
 2. torch_audiomentations provide cuda support enabling GPU computation.
 '''
 
+class ApplyAugmentations:
+    def __init__(self, augmentations, device=None):
+        """
+        Initializes the augmentation class with the list of augmentations to apply.
+
+        Parameters
+        ----------
+        augmentations : list
+            List of names of augmentations to apply.
+        device : torch.device, optional
+            Device to which the data should be moved (default is None).
+        """
+        self.augmentations = augmentations
+        self.device = device
+
+    def apply(self, data):
+        """
+        Applies the selected augmentations to the given data.
+
+        Parameters
+        ----------
+        data : torch.Tensor
+            The data to which augmentations will be applied.
+
+        Returns
+        -------
+        torch.Tensor
+            The augmented data.
+        """
+        if 'pitchshift' in self.augmentations:
+            data = self.pitch_shift(data)
+        if 'timeshift' in self.augmentations:
+            data = self.time_shift(data)
+        if 'addnoise' in self.augmentations:
+            data = self.add_noise(data)
+        # Apply more augmentations as needed
+        return data
+
+    def pitch_shift(self, data):
+        """
+        Applies pitch shift augmentation to the data.
+
+        Parameters
+        ----------
+        data : torch.Tensor
+            The data to augment.
+
+        Returns
+        -------
+        torch.Tensor
+            The pitch-shifted data.
+        """
+
+        return data
+
+    def time_shift(self, data):
+        """
+        Applies time shift augmentation to the data.
+
+        Parameters
+        ----------
+        data : torch.Tensor
+            The data to augment.
+
+        Returns
+        -------
+        torch.Tensor
+            The time-shifted data.
+        """
+
+        return data
+
+    def add_noise(self, data):
+        """
+        Adds noise to the data.
+
+        Parameters
+        ----------
+        data : torch.Tensor
+            The data to augment.
+
+        Returns
+        -------
+        torch.Tensor
+            The data with added noise.
+        """
+
+        return data
