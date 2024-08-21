a42bc67c994a7b324a1e094051118327f9933b75
nbrc <47108281+nbrochec@users.noreply.github.com>
2024-08-21 15:21:45 +0900

    Update utils.py
    Remove the old customLogMelSpectrogram class.

--- utils.py
+++ utils.py
@@ -23,61 +23,6 @@ import torch.nn.functional as Fnn
 from torch.utils.data import Dataset, DataLoader
 from pytorch_balanced_sampler.sampler import SamplerFactory
 
-class customLogMelSpectrogram():
-    def __init__(self, sample_rate=24000, n_fft=2048, win_length=None, hop_length=512, n_mels=128, f_min=150, f_max=None):
-        self.sample_rate = sample_rate
-        self.n_fft = n_fft
-        self.win_length = win_length if win_length is not None else n_fft
-        self.hop_length = hop_length
-        self.n_mels = n_mels
-        self.f_min = f_min
-        self.f_max = f_max if f_max is not None else sample_rate / 2
-        
-        self.mel_scale = Taudio.MelSpectrogram(
-            sample_rate=self.sample_rate,
-            n_fft=self.n_fft,
-            win_length=self.win_length,
-            hop_length=self.hop_length,
-            n_mels=self.n_mels,
-            f_min=self.f_min,
-            f_max=self.f_max,
-            power=2.0
-        )
-
-        self.amplitude_to_db = Taudio.AmplitudeToDB(stype='power', top_db=80.0)
-    
-    def __call__(self, waveform):
-        mel_spec = self.mel_scale(waveform)
-        mel_spec_db = self.amplitude_to_db(mel_spec)
-        mel_spec_db = Fnn.normalize(mel_spec_db, dim=1)
-        mel_spec_db = Fnn.normalize(mel_spec_db, dim=2)
-        return mel_spec_db
-    
-    def pad_segment(self, segment, current_segment_length, segment_length):
-        pad_length = segment_length - current_segment_length
-        segment = Fnn.pad(segment, (0, pad_length), mode='constant', value=0)
-        return segment
-        
-    def process_segment(self, waveform, segment_length=7680):
-        """Divides the waveform into segments of a fixed length and applies the Mel transformation."""
-        segments = []
-        total_length = waveform.size(1)
-        # print(total_length)
-
-        if total_length < segment_length:
-            pad_length = segment_length - total_length
-            waveform = Fnn.pad(waveform, (0, pad_length), mode='constant', value=0)
-            total_length = waveform.size(1)
-        
-        for start in range(0, total_length - segment_length + 1, segment_length):
-            segment = waveform[:, start:start + segment_length]
-            mel_spec_db = self.__call__(segment)[..., :-1] # manually remove the last frame to keep 15 frames only
-            segments.append(mel_spec_db)
-
-        stacked_segments = torch.stack(segments, dim=0)
-        return stacked_segments
-    
-
 class SilenceRemover:
     def __init__(self, silence_threshold=1e-4, min_silence_len=0.1):
         """
