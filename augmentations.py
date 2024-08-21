a213f8caf18dc166a586101314d3fdca2f4f10f9
nbrc <47108281+nbrochec@users.noreply.github.com>
2024-08-21 17:49:42 +0900

    Update augmentations.py
    Implementation of the following audio augmentations:
    - pitchshift
    - addcolornoise
    - shift
    - polarityinversion
    - gain
    - highpassfilter
    - lowpassfilter
    
    Add the possibility of applying the entire augmentations with argument 'all.'

--- augmentations.py
+++ augmentations.py
@@ -11,16 +11,16 @@
 #############################################################################
 
 import torch
-import torch_audiomentations
+from torch_audiomentations import PitchShift, AddColorNoise, Shift, PolarityInversion, Gain, HighPassFilter, LowPassFilter
 
 '''
 Principally using torch_audiomentations because:
-1. Augmentations are generated inside the training loop.
+1. Augmentations are applied inside the training loop.
 2. torch_audiomentations provide cuda support enabling GPU computation.
 '''
 
 class ApplyAugmentations:
-    def __init__(self, augmentations, device=None):
+    def __init__(self, augmentations, sample_rate, device=None):
         """
         Initializes the augmentation class with the list of augmentations to apply.
 
@@ -32,6 +32,7 @@ class ApplyAugmentations:
             Device to which the data should be moved (default is None).
         """
         self.augmentations = augmentations
+        self.sr = sample_rate
         self.device = device
 
     def apply(self, data):
@@ -48,62 +49,129 @@ class ApplyAugmentations:
         torch.Tensor
             The augmented data.
         """
-        if 'pitchshift' in self.augmentations:
-            data = self.pitch_shift(data)
-        if 'timeshift' in self.augmentations:
-            data = self.time_shift(data)
-        if 'addnoise' in self.augmentations:
-            data = self.add_noise(data)
+        if self.device:
+            data = data.to(self.device)
 
-        return data
+        # Prepare to accumulate the augmented samples
+        num_augmentations = len(self.augmentations)
+        batch_size = data.size(0)
+        augmented_data_list = []
+
+        # Apply each augmentation and store results in a list
+        for augmentation in self.augmentations:
+            if augmentation == 'pitchshift':
+                augmented_data_list.append(self.pitch_shift(data))
+            elif augmentation == 'timeshift':
+                augmented_data_list.append(self.time_shift(data))
+            elif augmentation == 'addnoise':
+                augmented_data_list.append(self.add_noise(data))
+            elif augmentation == 'polarityinversion':
+                augmented_data_list.append(self.polarity_inversion(data))
+            elif augmentation == 'gain':
+                augmented_data_list.append(self.gain(data))
+            elif augmentation == 'highpassfilter':
+                augmented_data_list.append(self.highpassfilter(data))
+            elif augmentation == 'lowpassfilter':
+                augmented_data_list.append(self.lowpassfilter(data))
+            elif augmentation == 'all':
+                augmented_data_list.append(self.pitch_shift(data))
+                augmented_data_list.append(self.time_shift(data))
+                augmented_data_list.append(self.add_noise(data))
+                augmented_data_list.append(self.polarity_inversion(data))
+                augmented_data_list.append(self.gain(data))
+                augmented_data_list.append(self.highpassfilter(data))
+                augmented_data_list.append(self.lowpassfilter(data))
+
+        # Stack all augmented samples
+        augmented_data = torch.cat(augmented_data_list, dim=0)
+
+        return augmented_data
 
     def pitch_shift(self, data):
         """
         Applies pitch shift augmentation to the data.
-
-        Parameters
-        ----------
-        data : torch.Tensor
-            The data to augment.
-
-        Returns
-        -------
-        torch.Tensor
-            The pitch-shifted data.
         """
-
+        transform=[
+                PitchShift(
+                    min_semitones=-24,
+                    max_semitones=24,
+                    p=1
+                )
+            ]
+        data = transform(data, sample_rate=self.sr)
         return data
 
     def time_shift(self, data):
         """
         Applies time shift augmentation to the data.
-
-        Parameters
-        ----------
-        data : torch.Tensor
-            The data to augment.
-
-        Returns
-        -------
-        torch.Tensor
-            The time-shifted data.
         """
-
+        transform=[
+            Shift(
+                rollover=True,
+                shift_unit='samples',
+                p=1
+            )
+        ]
+        data = transform(data)
         return data
 
     def add_noise(self, data):
         """
         Adds noise to the data.
-
-        Parameters
-        ----------
-        data : torch.Tensor
-            The data to augment.
-
-        Returns
-        -------
-        torch.Tensor
-            The data with added noise.
         """
-
+        transform=[
+            AddColorNoise(
+                f_decay=0,
+                p=1
+            )
+        ]
+        data = transform(data)
+        return data
+    
+    def polarity_inversion(data):
+        """
+        Applies a polarity inversion to the data.
+        """
+        transform=[
+            PolarityInversion(
+                p=1
+            )
+        ]
+        data = transform(data)
+        return data
+    
+    def gain(data):
+        """
+        Multiply the audio by a random amplitude factor to reduce or increase the volume.
+        """
+        transform=[
+            Gain(
+                p=1
+            )
+        ]
+        data = transform(data)
+        return data
+    
+    def highpassfilter(data):
+        """
+        Apply high-pass filtering to the input audio.
+        """
+        transform=[
+            HighPassFilter(
+                p=1
+            )
+        ]
+        data = transform(data)
+        return data
+    
+    def lowpassfilter(data):
+        """
+        Apply low-pass filtering to the input audio.
+        """
+        transform=[
+            LowPassFilter(
+                p=1
+            )
+        ]
+        data = transform(data)
         return data
