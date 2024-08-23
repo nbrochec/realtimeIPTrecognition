8327c596b0d85a1f23c38a63b3d8901b68a6221b
nbrc <47108281+nbrochec@users.noreply.github.com>
2024-08-23 14:56:57 +0900

    Update train.py
    Rewrite of train.py to make it readable.

--- train.py
+++ train.py
@@ -10,61 +10,102 @@
 # Script to train the model
 #############################################################################
 
-import argparse, sys, math, os
-import torch, torchaudio, h5py
-from glob import glob
-from os.path import join
+import argparse
+import os
+import sys
+import pandas as pd
+import torch
+import torchaudio
+from torch.utils.data import DataLoader
 
 from models import *
 from utils import *
+# Constants
+SEGMENT_LENGTH = 7680
 
-from torch.utils.data import Dataset, DataLoader
+def parse_arguments():
+    """Parse command-line arguments."""
+    parser = argparse.ArgumentParser(description='This script launches the training process.')
+    parser.add_argument('--csv_file', type=str, default='dataset_split.csv', help='Path to dataset CSV file.')
+    parser.add_argument('--epochs', type=int, default=100, help='Number of training epochs.')
+    parser.add_argument('--config', type=str, default='v2', help='CNN configuration version.')
+    parser.add_argument('--gpu', type=int, default=0, help='GPU device number.')
+    parser.add_argument('--sr', type=int, default=24000, help='Sampling rate of audio files.')
+    parser.add_argument('--augment', type=str, default='pitchshift', help='Augmentation methods to apply.')
+    parser.add_argument('--early_stopping', type=bool, default=False, help='Use early stopping.')
+    parser.add_argument('--reduceLR', type=bool, default=False, help='Reduce learning rate on plateau.')
+    parser.add_argument('--lr', type=float, default=0.001, help='Learning rate.')
+    parser.add_argument('--fmin', type=int, default=150, help='Minimum frequency for logmelspec analysis.')
+    return parser.parse_args()
 
-parser = argparse.ArgumentParser(description='train CNN model for RT-IPT-R')
-parser.add_argument('--csv_file', type=str, help='dataset directory where h5 files are saved', default='dataset_split.csv')
-parser.add_argument('--epochs', type=int, help='number of train epochs', default=100)
-parser.add_argument('--config', type=str, help='version of the CNN', default='v2')
-parser.add_argument('--device', type=str, help='gpu device', default='cpu')
-parser.add_argument('--gpu', type=int, help='gpu device number', default=0)
-parser.add_argument('--sr', type=int, help='sampling rate', default=24000)
-parser.add_argument('--augment', type=str, help='augmentations', default='pitchshift')
-parser.add_argument('--early_stopping', type=bool, help='early stopping', default=False)
-parser.add_argument('--reduceLR', type=bool, help='reduce LR on plateau', default=False)
-parser.add_argument('--lr', type=float, help='learning rate', default=0.001)
-parser.add_argument('--fmin', type=int, help='select min frequency for logmelspec analysis', default=150)
+def get_device():
+    """Automatically select the device."""
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    print(f'This script use {device} as torch device.')
+    return device
 
-"""
-Command example:
-python train.py --config v1 --augment pitchshift --early_stopping True --reduceLR True 
-"""
+def get_num_labels_from_csv(csv_file_path):
+    """Extract the number of unique labels from the CSV file."""
+    df = pd.read_csv(csv_file_path)
+    return len(df['label'].unique())
 
-args = parser.parse_args()
-device = args.device
+def collate_fn(batch, device):
+    """Load segments and labels on the torch device"""
+    segments, labels = zip(*batch)
+    segments = [s.to(device) for s in segments]
+    labels = [l.to(device) for l in labels]
+    return segments, labels
 
-cwd = os.getcwd()+'/data/dataset/'
-csv_file_path = os.path.join(cwd, args.csv_file)
-segment_length = 7680
+def prepare_data(args, device):
+    """Prepare data loaders."""
+    # Define file paths and dataset parameters
+    cwd = os.path.join(os.getcwd(), 'data', 'dataset')
+    csv_file_path = os.path.join(cwd, args.csv_file)
+
+    # Load datasets
+    train_dataset = ProcessDataset('train', csv_file_path, args.sr, SEGMENT_LENGTH)
+    test_dataset = ProcessDataset('test', csv_file_path, args.sr, SEGMENT_LENGTH)
+    val_dataset = ProcessDataset('val', csv_file_path, args.sr, SEGMENT_LENGTH)
+
+    # Create data loaders
+    train_loader = BalancedDataLoader(train_dataset, device).get_dataloader()
+    test_loader = DataLoader(test_dataset, batch_size=64, collate_fn=lambda x: collate_fn(x, device))
+    val_loader = DataLoader(val_dataset, batch_size=64, collate_fn=lambda x: collate_fn(x, device))
+
+    print('Data successfully loaded into DataLoaders.')
+
+    return train_loader, test_loader, val_loader
+
+def prepare_model(args, device):
+    """Prepare the model."""
+    # Define file paths
+    cwd = os.path.join(os.getcwd(), 'data', 'dataset')
+    csv_file_path = os.path.join(cwd, args.csv_file)
+
+    # Get number of labels
+    num_labels = get_num_labels_from_csv(csv_file_path)
+
+    # Load model
+    model = LoadModel().get_model(args.config, num_labels).to(device)
+    summary = ModelSummary(model, num_labels, args.config)
+    summary.print_summary()
+
+    # Test model
+    tester = ModelTester(model, input_shape=(1, 1, SEGMENT_LENGTH), device=device)
+    output = tester.test()
+
+    if output.size(1) != num_labels:
+        print("Error: Output dimension does not match the number of classes.")
+        sys.exit(1)
+
+    return model
 
 if __name__ == '__main__':
-    train_dataset = ProcessDataset('train', csv_file_path, args.sr, segment_length)
-
-    train_loader = BalancedDataLoader(train_dataset).get_dataloader()
-
-# model = LoadModel().get_model(args.config, num_labels).to(args.device)
-# summary = ModelSummary(model, num_labels, args.config)
-# summary.print_summary()
-
-# tester = ModelTester(model, input_shape=(1, 1, 7680), device=args.device)
-# output = tester.test()
-# if output.size(1) == num_labels:
-#     pass
-# else:
-#     print("Error: output dimension do not correspond to the number of classes")
-#     sys.exit(1)
-
-# Training loop (to be completed with validation data)
-# augmentations = args.augment.split()
-# augmentation_instance = ApplyAugmentations(augmentations)
+    args = parse_arguments()
+    device = get_device()
+    train_loader, test_loader, val_loader = prepare_data(args, device)
+    model = prepare_model(args, device)
+    
 
 # for batch_data, batch_labels in balanced_loader:
 #     # Apply augmentations
