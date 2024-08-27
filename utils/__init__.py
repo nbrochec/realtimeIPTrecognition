49b1aeb8a16f652ff313ebfbec51bbb902997c5b
nbrc <47108281+nbrochec@users.noreply.github.com>
2024-08-27 15:21:07 +0900

    Update __init__.py
    Add ModelTrainer class.

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,8 +1,8 @@
-from utils.utils import DirectoryManager, DatasetSplitter, BalancedDataLoader, PreprocessAndSave, DatasetValidator, ProcessDataset
+from utils.utils import DirectoryManager, DatasetSplitter, BalancedDataLoader, PreprocessAndSave, DatasetValidator, ProcessDataset, ModelTrainer
 from utils.augmentation import ApplyAugmentations
 from externals.pytorch_balanced_sampler.sampler import SamplerFactory
 
 __all__=[
-    'DirectoryManager', 'DatasetSplitter', 'BalancedDataLoader', 'PreprocessAndSave', 'DatasetValidator','ProcessDataset',
+    'DirectoryManager', 'DatasetSplitter', 'BalancedDataLoader', 'PreprocessAndSave', 'DatasetValidator','ProcessDataset', 'ModelTrainer',
     'ApplyAugmentations', 'SamplerFactory'
 ]
