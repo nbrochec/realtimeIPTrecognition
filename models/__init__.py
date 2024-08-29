8e3fd429ea77354a38a848fd485876bd5933c048
nbrc <47108281+nbrochec@users.noreply.github.com>
2024-08-29 21:55:45 +0900

    Update __init__.py
    Add PrepareModel class

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,9 @@
 from .models import v1, v2, one_residual, two_residual, transformer
 from .layers import custom1DCNN, custom2DCNN, LogMelSpectrogramLayer
-from .utils import LoadModel, ModelSummary, ModelTester, ModelInit, ModelTrainer
+from .utils import LoadModel, ModelSummary, ModelTester, ModelInit, ModelTrainer, PrepareModel
 
 __all__=[
     'v1', 'v2', 'one_residual', 'two_residual', 'transformer',
     'custom1DCNN', 'custom2DCNN', 'LogMelSpectrogramLayer',
-    'LoadModel', 'ModelSummary', 'ModelTester', 'ModelInit', 'ModelTrainer'
+    'LoadModel', 'ModelSummary', 'ModelTester', 'ModelInit', 'ModelTrainer', 'PrepareModel'
 ]
