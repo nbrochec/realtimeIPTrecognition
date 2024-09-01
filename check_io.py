59bd2d0fe5f5280d7cb3016331c818cde20470fa
nbrc <47108281+nbrochec@users.noreply.github.com>
2024-09-02 00:31:29 +0900

    Create check_io.py
    This script allows the user to print in the terminal the available audio input devices of his computer.

---
+++ check_io.py
@@ -0,0 +1,21 @@
+#############################################################################
+# check_io.py 
+# Nicolas Brochec, TOKYO UNIVERSITY OF THE ARTS
+# 東京藝術大学音楽音響創造科
+# ERC Reach
+# GNU General Public License v3.0
+#############################################################################
+# Code description:
+# Print available audio input devices for PyAudio 
+#############################################################################
+
+import pyaudio
+
+p = pyaudio.PyAudio()
+info = p.get_host_api_info_by_index(0)
+numdevices = info.get('deviceCount')
+
+if __name__ == '__main__':
+    for i in range(0, numdevices):
+        if (p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
+            print("Input Device ID ", i, " - ", p.get_device_info_by_host_api_device_index(0, i).get('name'))
