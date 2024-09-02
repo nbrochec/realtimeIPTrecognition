f1ed2c73f2db4a73b9129332c9112c6e2202a26a
nbrc <47108281+nbrochec@users.noreply.github.com>
2024-09-02 16:33:54 +0900

    Update check_io.py
    Typo correction.

--- check_io.py
+++ check_io.py
@@ -1,6 +1,7 @@
 #############################################################################
 # check_io.py 
-# Nicolas Brochec, TOKYO UNIVERSITY OF THE ARTS
+# Nicolas Brochec
+# TOKYO UNIVERSITY OF THE ARTS
 # 東京藝術大学音楽音響創造科
 # ERC Reach
 # GNU General Public License v3.0
