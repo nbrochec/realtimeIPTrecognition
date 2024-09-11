a82d4c44f4cf67535d5da835120076e5a4388bb6
nbrc <47108281+nbrochec@users.noreply.github.com>
2024-09-11 14:23:50 +0200

    Update __init__.py
    Add model v3 to init.

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,4 @@
-from .models import v1, v2, one_residual, two_residual, transformer
+from .models import v1, v2, v3, one_residual, two_residual, transformer
 from .layers import custom1DCNN, custom2DCNN, LogMelSpectrogramLayer
 from .utils import LoadModel, ModelSummary, ModelTester, ModelInit, ModelTrainer, PrepareModel
 
