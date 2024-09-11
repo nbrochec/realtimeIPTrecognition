b4715e1413f2afb5f8eca8ccd14212df67724e0f
nbrc <47108281+nbrochec@users.noreply.github.com>
2024-09-11 14:24:13 +0200

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -3,7 +3,7 @@ from .layers import custom1DCNN, custom2DCNN, LogMelSpectrogramLayer
 from .utils import LoadModel, ModelSummary, ModelTester, ModelInit, ModelTrainer, PrepareModel
 
 __all__=[
-    'v1', 'v2', 'one_residual', 'two_residual', 'transformer',
+    'v1', 'v2', 'v3', 'one_residual', 'two_residual', 'transformer',
     'custom1DCNN', 'custom2DCNN', 'LogMelSpectrogramLayer',
     'LoadModel', 'ModelSummary', 'ModelTester', 'ModelInit', 'ModelTrainer', 'PrepareModel'
 ]
