7346dfc2995c3d77901b34636b2ed67028bbc6cf
nbrc <47108281+nbrochec@users.noreply.github.com>
2024-09-13 12:17:35 +0200

    Update preprocess.py
    Add val dir

--- preprocess.py
+++ preprocess.py
@@ -18,6 +18,7 @@ def parse_args():
     parser = argparse.ArgumentParser(description='Preprocess the dataset and save to disk a csv file.')
     parser.add_argument('--train_dir', type=str, help='Directory of training samples to preprocess.', default='train')
     parser.add_argument('--test_dir', type=str, help='Directory of test samples to preprocess.', default='test')
+    parser.add_argument('--val_dir', type=str, help='Directory of val samples to preprocess.', default=None)
     parser.add_argument('--val_split', type=str, help='Specify on which dataset the validation split would be made.', default='train')
     parser.add_argument('--val_ratio', type=float, help='Amount of validation samples.', default=0.2)
     parser.add_argument('--name', type=str, help='Name of the project.', required=True)
@@ -35,5 +36,5 @@ if __name__ == '__main__':
         print(f'Validation dataset will be created from the training dataset based on a {args.val_ratio} ratio.')
     elif args.val_split == 'test':
         print(f'Validation dataset will be created from the test dataset based on a {args.val_ratio} ratio.')
-    DatasetSplitter.split_train_validation(base_dir=base_dir, destination=csv_dir, train_dir=args.train_dir, test_dir=args.test_dir, val_ratio=args.val_ratio, val_split=args.val_split, name=args.name)
+    DatasetSplitter.split_train_validation(base_dir=base_dir, destination=csv_dir, train_dir=args.train_dir, test_dir=args.test_dir, val_dir=args.val_dir, val_ratio=args.val_ratio, val_split=args.val_split, name=args.name)
     DatasetValidator.validate_labels(os.path.join(csv_dir, f'{args.name}_dataset_split.csv'))
