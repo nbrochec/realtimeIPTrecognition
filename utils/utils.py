2ce8b72179d7a44bf216074b2a7c7400d19c9a7e
nbrc <47108281+nbrochec@users.noreply.github.com>
2024-09-14 17:31:07 +0200

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -332,10 +332,10 @@ class BalancedDataLoader:
         for segs, lbls in batch:
             segs = segs.unsqueeze(0)
             aug_segs = self.augmentations.apply(segs)
-            new_data = torch.cat((aug_segs, segs), dim=0)
+            new_data = torch.cat((aug_segs, segs), dim=0).to(self.device)
             
-            all_targets = torch.flatten(lbls.repeat(self.aug_nbr + 1, 1))
-            
+            all_targets = torch.flatten(lbls.repeat(self.aug_nbr + 1, 1)).to(self.device)
+
             segments.extend(new_data)
             labels.extend(all_targets)
 
