24e67b5395f063ee1e917e70c5682093e8631e1e
nbrc <47108281+nbrochec@users.noreply.github.com>
2024-09-23 14:43:38 +0200

    Update __init__.py
    Implement v2bis and oneDimension models.

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,9 @@
-from .models import v1, v2, v3, one_residual, two_residual, transformer
+from .models import v1, v2, v3, one_residual, two_residual, transformer, v2bis, oneDimension
 from .layers import custom1DCNN, custom2DCNN, LogMelSpectrogramLayer
 from .utils import LoadModel, ModelSummary, ModelTester, ModelInit, ModelTrainer, PrepareModel
 
 __all__=[
-    'v1', 'v2', 'v3', 'one_residual', 'two_residual', 'transformer',
+    'v1', 'v2', 'v3', 'one_residual', 'two_residual', 'transformer', 'v2bis', 'oneDimension',
     'custom1DCNN', 'custom2DCNN', 'LogMelSpectrogramLayer',
     'LoadModel', 'ModelSummary', 'ModelTester', 'ModelInit', 'ModelTrainer', 'PrepareModel'
 ]
