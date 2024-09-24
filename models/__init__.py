5f5a714788f5d8076688edf1743e4eecab0812bd
nbrc <47108281+nbrochec@users.noreply.github.com>
2024-09-24 12:47:14 +0200

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,9 @@
 from .models import v1, v2, v3, v2bis, v2_1d, v1_1d
-from .layers import custom1DCNN, custom2DCNN, LogMelSpectrogramLayer
+from .layers import custom1DCNN, custom2DCNN, LogMelSpectrogramLayer, EnvelopeExtractor
 from .utils import LoadModel, ModelSummary, ModelTester, ModelInit, ModelTrainer, PrepareModel
 
 __all__=[
     'v1', 'v2', 'v3', 'v2bis', 'v2_1d', 'v1_1d',
-    'custom1DCNN', 'custom2DCNN', 'LogMelSpectrogramLayer',
+    'custom1DCNN', 'custom2DCNN', 'LogMelSpectrogramLayer', 'EnvelopeExtractor',
     'LoadModel', 'ModelSummary', 'ModelTester', 'ModelInit', 'ModelTrainer', 'PrepareModel'
 ]
