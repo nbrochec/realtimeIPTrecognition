80a4dbf750c66a0d4950b06d3f21375093b5b7c9
nbrc <47108281+nbrochec@users.noreply.github.com>
2024-09-26 14:54:54 +0200

    Update augmentation.py
    Implement Offline and Online augmentations.

--- utils/augmentation.py
+++ utils/augmentation.py
@@ -11,113 +11,30 @@
 #############################################################################
 
 import torch
-from audiomentations import PitchShift, AddColorNoise, Shift, PolarityInversion, Gain, HighPassFilter, LowPassFilter, Mp3Compression, ClippingDistortion, BitCrush, AirAbsorption
+from audiomentations import PitchShift, AddColorNoise, Shift, PolarityInversion, Gain, HighPassFilter, Trim
+from audiomentations import LowPassFilter, Mp3Compression, ClippingDistortion, BitCrush, AirAbsorption, Aliasing
 import torch.nn.functional as F
 import numpy as np
-import librosa
+import librosa, random
 
-class ApplyAugmentations:
-    def __init__(self, augmentations, sample_rate, device):
-        """
-        Initializes the augmentation class with the list of augmentations to apply.
+class AudioOnlineTransforms:
+    def __init__(self, args):
+        self.sr = args.sr
+        self.augment = args.augment.split()
+        self.device = args.device
 
-        Parameters
-        ----------
-        augmentations : list
-            List of names of augmentations to apply.
-        sample_rate : int
-            Sample rate.
-        device : torch.device
-            The device to which the data will be moved.
-        """
-        self.augmentations = augmentations
-        self.sr = sample_rate
-        self.device = device
-
-    def apply(self, data):
-        """
-        Applies the selected augmentations to the given data.
-        """
-        data = data.cpu().squeeze(1).detach().numpy()
-
-        original_size = data.shape[1]
-        augmented_data_list = []
-
-        augmentations_dict = {
-            'pitchshift': self.pitch_shift,
-            'lb_pitchshift': self.lb_pitch_shift,
-            'timeshift': self.shift,
-            # 'addnoise': self.add_noise,
-            'polarityinversion': self.polarity_inversion,
-            # 'gain': self.gain,
-            'hpf': self.highpassfilter,
-            'lpf': self.lowpassfilter,
-            'clipping': self.clippingdisto,
-            'bitcrush': self.bitcrush,
-            'airabso': self.airabso,
-            'gaussnoise': self.gaussnoise
-        }
-
-        for augmentation in self.augmentations:
-            if augmentation == 'all':
-                aug_data = [
-                    augmentations_dict['pitchshift'](data),
-                    augmentations_dict['lb_pitchshift'](data),
-                    augmentations_dict['timeshift'](data),
-                    # augmentations_dict['addnoise'](data),
-                    augmentations_dict['polarityinversion'](data),
-                    # augmentations_dict['gain'](data),
-                    augmentations_dict['hpf'](data),
-                    augmentations_dict['lpf'](data),
-                    augmentations_dict['clipping'](data),
-                    augmentations_dict['bitcrush'](data),
-                    augmentations_dict['airabso'](data),
-                    augmentations_dict['gaussnoise'](data)
-                ]
-                augmented_data_list.extend(aug_data)
-                continue 
-
-            aug_data = augmentations_dict.get(augmentation, lambda x: x)(data)
-            aug_data = self.pad_or_trim(aug_data, original_size)
-            augmented_data_list.append(aug_data)
-
-        augmented_data_list = [torch.tensor(d).unsqueeze(1) for d in augmented_data_list] 
-        augmented_data = torch.cat(augmented_data_list, dim=0).to(self.device)
-        augmented_data = augmented_data.to(torch.float32)
-        return augmented_data
-
-    def pad_or_trim(self, data, original_size):
-        current_size = data.shape[1]
-        
-        if current_size > original_size:
-            data = data[..., :original_size]
-        elif current_size < original_size:
-            padding = (0, original_size - current_size)
-            data = np.pad(data, pad_width=padding, mode='constant', constant_values=0)
-        
-        return data
-
-    def pitch_shift(self, data):
-        transform = PitchShift(min_semitones=-12.0, max_semitones=12.0, p=1)
-        return transform(data, sample_rate= self.sr)
+    def tensor_to_array(self, data):
+        data_numpy = data.cpu().squeeze(1).detach().numpy()
+        return data_numpy
     
-    def lb_pitch_shift(self, data):
-        random_number = np.random.uniform(-100, 100)
-        random_tuning = 440 + random_number
-        change_tuning = librosa.A4_to_tuning(random_tuning)
-        data = librosa.effects.pitch_shift(data, sr=self.sr, n_steps=change_tuning, bins_per_octave=12)
-        return data
-
     def shift(self, data):
         transform = Shift(rollover=True, p=1)
         return transform(data, sample_rate= self.sr)
     
-    def gaussnoise(self, data):
-        n_channels, length = data.shape
-        data = data + np.random.normal(0, 0.01, size=(n_channels, length)) * np.random.randn(n_channels, length)
-        data = data[:, :length]
-        return data
-
+    def pitch_shift(self, data):
+        transform = PitchShift(min_semitones=-12.0, max_semitones=12.0, p=1)
+        return transform(data, sample_rate= self.sr)
+    
     def add_noise(self, data):
         transform = AddColorNoise(p=1)
         return transform(data, sample_rate=self.sr)
@@ -150,8 +67,103 @@ class ApplyAugmentations:
         transform = AirAbsorption(p=1)
         return transform(data, sample_rate=self.sr)
     
-    def get_aug_nbr(self):
-        aug_nbr = len(self.augmentations)
-        if self.augmentations == ['all']:
-            aug_nbr = 10
-        return aug_nbr
+    def aliasing(self, data):
+        transform = Aliasing(min_sample_rate=8000, max_sample_rate=self.sr//2, p=1)
+        return transform(data, sample_rate=self.sr)
+    
+    def mp3comp(self, data):
+        transform = Mp3Compression(p=1)
+        return transform(data, sample_rate=self.sr)
+    
+    def trim(self, data):
+        transform = Trim(top_db=30.0,p=1)
+        return transform(data, sample_rate=self.sr)
+
+    def pad_or_trim(self, data, original_size):
+        current_size = data.shape[1]
+        if current_size > original_size:
+            data = data[..., :original_size]
+        elif current_size < original_size:
+            padding = (0, original_size - current_size)
+            data = np.pad(data, pad_width=padding, mode='constant', constant_values=0)
+        return data
+    
+    def __call__(self, data):
+        data_numpy = self.tensor_to_array(data)
+        original_size = data_numpy.shape[1]
+        aug_data_list = []
+
+        aug_dict = {
+            'pitchshift': self.pitch_shift,
+            'timeshift': self.shift,
+            'polarityinversion': self.polarity_inversion,
+            'hpf': self.highpassfilter,
+            'lpf': self.lowpassfilter,
+            'clipping': self.clippingdisto,
+            'bitcrush': self.bitcrush,
+            'airabso': self.airabso,
+            'aliasing': self.aliasing,
+            'mp3comp': self.mp3comp,
+            'trim': self.trim
+        }
+
+        for augmentation in self.augment:
+            if augmentation in aug_dict and np.random.rand() < 0.5:
+                data_numpy = aug_dict[augmentation](data_numpy)
+
+        aug_data = self.pad_or_trim(data_numpy, original_size)
+        aug_data_list.append(aug_data)
+
+        aug_data_tensor = torch.tensor(aug_data).unsqueeze(1).to(self.device).to(torch.float32)
+
+        return aug_data_tensor
+    
+class AudioOfflineTransforms:
+    def __init__(self, args):
+        self.sr = args.sr
+        self.device = args.device
+
+    def tensor_to_array(self, data):
+        data_numpy = data.cpu().squeeze(1).detach().numpy()
+        return data_numpy
+
+    def custom_detune(self, data):
+        random_tuning= np.random.uniform(-100, 100) + 440
+        change_tuning = librosa.A4_to_tuning(random_tuning)
+        data = librosa.effects.pitch_shift(data, sr=self.sr, n_steps=change_tuning, bins_per_octave=12)
+        return data
+
+    def custom_gaussnoise(self, data):
+        n_channels, length = data.shape
+        data = data + np.random.normal(0, 0.01, size=(n_channels, length)) * np.random.randn(n_channels, length)
+        data = data[:, :length]
+        return data
+    
+    def pad_or_trim(self, data, original_size):
+        current_size = data.shape[1]
+        if current_size > original_size:
+            data = data[..., :original_size]
+        elif current_size < original_size:
+            padding = (0, original_size - current_size)
+            data = np.pad(data, pad_width=padding, mode='constant', constant_values=0)
+        return data
+    
+    def __call__(self, data):
+        data_numpy = self.tensor_to_array(data)
+        original_size = data_numpy.shape[1]
+
+        aug_dict = {
+            'detune': self.custom_detune,
+            'gaussnoise': self.custom_gaussnoise,
+        }
+
+        detuned = aug_dict['detune'](data_numpy)
+        noised = aug_dict['gaussnoise'](data_numpy)
+
+        aug_detuned = self.pad_or_trim(detuned, original_size)
+        aug_noised = self.pad_or_trim(noised, original_size)
+
+        aug1 = torch.tensor(aug_detuned).to(self.device).to(torch.float32)
+        aug2 = torch.tensor(aug_noised).to(self.device).to(torch.float32)
+
+        return aug1, aug2
