1067e06d43e3db5a54501416ff0d247da4ef53fe
nbrc <47108281+nbrochec@users.noreply.github.com>
2024-09-26 14:55:18 +0200

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,12 +1,12 @@
 from utils.constants import SEGMENT_LENGTH
 from utils.utils import DirectoryManager, DatasetSplitter, BalancedDataLoader, DatasetValidator, ProcessDataset, PrepareData, SaveResultsToTensorboard, SaveResultsToDisk, SaveYAML, GetDevice, Dict2MDTable
-from utils.augmentation import ApplyAugmentations
+from utils.augmentation import AudioOfflineTransforms, AudioOnlineTransforms
 from externals.pytorch_balanced_sampler.sampler import SamplerFactory
 from utils.rt import Resample, SendOSCMessage, PredictionBuffer, MakeInference
 
 __all__=[
     'DirectoryManager', 'DatasetSplitter', 'BalancedDataLoader', 'DatasetValidator','ProcessDataset', 'PrepareData', 'SaveResultsToTensorboard', 'SaveResultsToDisk', 'SaveYAML', 'GetDevice', 'Dict2MDTable',
-    'ApplyAugmentations', 'SamplerFactory',
+    'AudioOfflineTransforms', 'AudioOnlineTransforms','SamplerFactory',
     'Resample', 'SendOSCMessage', 'PredictionBuffer', 'MakeInference',
     'SEGMENT_LENGTH'
 ]
