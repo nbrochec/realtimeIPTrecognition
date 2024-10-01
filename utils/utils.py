96e556109772b9b29b3022353ddcd1c4ade435a6
nbrc <47108281+nbrochec@users.noreply.github.com>
2024-10-01 15:06:41 +0200

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -262,9 +262,11 @@ class ProcessDataset:
             num_samples = waveform.size(1)
 
             if self.padding == 'minimal':
-                extra_length = self.segment_length - num_samples
-                silence = torch.zeros((waveform.size(0), extra_length))
-                waveform = torch.cat((waveform, silence), dim=1)
+                if num_samples <= self.segment_length:
+                    extra_length = self.segment_length - num_samples
+                    silence = torch.zeros((waveform.size(0), extra_length))
+                    waveform = torch.cat((waveform, silence), dim=1)
+                    num_samples = waveform.size(1)
 
 
             augmenter = AudioOfflineTransforms(self.args)
