426a25c5f2d19e682e8b67b753ab01dc545f04f3
nbrc <47108281+nbrochec@users.noreply.github.com>
2024-10-01 15:08:02 +0200

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -266,10 +266,7 @@ class ProcessDataset:
                     extra_length = self.segment_length - num_samples
                     silence = torch.zeros((waveform.size(0), extra_length))
                     waveform = torch.cat((waveform, silence), dim=1)
-
-                # noise_length = waveform.size(1) 
-                # noise = torch.randn((waveform.size(0), noise_length)) * 1e-9
-                # waveform += noise
+                    num_samples = waveform.size(1)
 
             augmenter = AudioOfflineTransforms(self.args)
 
