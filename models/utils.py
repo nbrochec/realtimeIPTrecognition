3fe78e9cc45d17d0d11ffbb8594c815ecf325e97
nbrc <47108281+nbrochec@users.noreply.github.com>
2024-10-12 11:05:54 +0900

    Implement v1_mi6_env2_stacks7

--- models/utils.py
+++ models/utils.py
@@ -15,7 +15,7 @@ import humanize
 import sys
 import pandas as pd
 
-from models import v1, v2, v3, v2_1d, v1_mi4, v1_mi6_env2_lstm, v1_mi6_env2_mod, v1_mi6_env2_lstm_new, v1_mi6_hpss, v1_mi6_hpss_only, v1_mi6_env2_stack, v1_mi6_env2_stacks
+from models import v1, v2, v3, v2_1d, v1_mi4, v1_mi6_env2_lstm, v1_mi6_env2_mod, v1_mi6_env2_lstm_new, v1_mi6_hpss, v1_mi6_hpss_only, v1_mi6_env2_stack, v1_mi6_env2_stacks, v1_mi6_env2_stacks7
 from models import v1_mi6, v1_mi6_env2, v1_mi5_env2
 
 from tqdm import tqdm
@@ -53,7 +53,8 @@ class LoadModel:
             'v1_mi6_hpss': v1_mi6_hpss,
             'v1_mi6_hpss_only': v1_mi6_hpss_only,
             'v1_mi6_env2_stack': v1_mi6_env2_stack,
-            'v1_mi6_env2_stacks': v1_mi6_env2_stacks
+            'v1_mi6_env2_stacks': v1_mi6_env2_stacks,
+            'v1_mi6_env2_stacks7': v1_mi6_env2_stacks7
         }
     
     def get_model(self, model_name, output_nbr, args):
