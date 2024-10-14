3c1d43864e2a52d2235836cf387f7305c21896f1
nbrc <47108281+nbrochec@users.noreply.github.com>
2024-10-14 10:50:32 +0900

    Update utils.py
    Remove unused models

--- models/utils.py
+++ models/utils.py
@@ -15,8 +15,7 @@ import humanize
 import sys
 import pandas as pd
 
-from models import v1, v2, v3, v2_1d, v1_mi4, v1_mi6_env2_lstm, v1_mi6_env2_rdc, v1_mi6_env2_lstm_rdc
-from models import v1_mi6, v1_mi6_env2, v1_mi5_env2
+from models import v1, v2, v3
 
 from tqdm import tqdm
 
@@ -42,14 +41,6 @@ class LoadModel:
             'v1': v1,
             'v2': v2,
             'v3': v3,
-            'v2_1d': v2_1d,
-            'v1_mi4': v1_mi4,
-            'v1_mi6': v1_mi6,
-            'v1_mi6_env2': v1_mi6_env2,
-            'v1_mi5_env2': v1_mi5_env2,
-            'v1_mi6_env2_lstm': v1_mi6_env2_lstm,
-            'v1_mi6_env2_rdc': v1_mi6_env2_rdc,
-            'v1_mi6_env2_lstm_rdc': v1_mi6_env2_lstm_rdc
         }
     
     def get_model(self, model_name, output_nbr, args):
