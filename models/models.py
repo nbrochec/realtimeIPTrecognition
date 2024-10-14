e88e05839e17f6ff1cc7fb9c00789ea707782185
nbrc <47108281+nbrochec@users.noreply.github.com>
2024-10-14 20:58:45 +0900

    Update models.py

--- models/models.py
+++ models/models.py
@@ -1691,10 +1691,10 @@ class v1_mi6_env2_mod_new_stack(nn.Module):
         # change dilation
         # augment kernel
         return nn.Sequential(
-            custom1DCNN(1, 40, 20, "same", 1), #old 7 puis 15
+            custom1DCNN(1, 40, 40, "same", 1), #old 7 puis 15 puis 20
             nn.AvgPool1d(16),
             nn.Dropout1d(0.25),
-            custom1DCNN(40, 40, 15, "same", 2), #old 5 puis 10
+            custom1DCNN(40, 40, 20, "same", 2), #old 5 puis 10 puis 15
             nn.AvgPool1d(8),
             nn.Dropout1d(0.25),
             custom1DCNN(40, 80, 7, "same", 3), #old 3 puis 5
