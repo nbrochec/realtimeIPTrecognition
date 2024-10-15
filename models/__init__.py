301d7edbb8e94cb0c28c3b0bbd938273f5b1675d
nbrc <47108281+nbrochec@users.noreply.github.com>
2024-10-16 07:41:37 +0900

    Remove trash models

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,9 @@
-from .models import v1, v2, v3, v2_1d, v1_mi4, v1_mi6, v1_mi6_env2, v1_mi5_env2, v1_mi6_env2_lstm, v1_mi6_env2_mod, v1_mi6_env2_lstm_new, v1_mi6_hpss, v1_mi6_hpss_only, v1_mi6_env2_stack, v1_mi6_env2_mod_stacks, v1_mi6_env2_stacks7, v1_mi6_env2_mod_new, v1_mi6_mod_stacks7, v1_mi6_env2_mod_full_stack, context_net, v1_mi6_env2_mod_new_stack, v1_mi6_env2_mod_new_stack_onset, v1_mi6_env2_mod_new_stack8, ARNModel_env2_stack, ARBModel_stack, ARN
+from .models import v1, v2, v3, v1_mi6, v1_mi6_env2_mod_stacks, v1_mi6_env2_mod_new, v1_mi6_env2_mod_new_stack, v1_mi6_env2_mod_new_stack8, ARNModel_env2_stack, ARBModel_stack, v1_mi6_env2_mod_new_stack8x8
 from .layers import custom1DCNN, custom2DCNN, LogMelSpectrogramLayer, EnvelopeFollowingLayerTorchScript, ARB, customARB, ARB1d
 from .utils import LoadModel, ModelSummary, ModelTester, ModelInit, ModelTrainer, PrepareModel
 
 __all__=[
-    'v1', 'v2', 'v3', 'v2_1d', 'v1_mi4', 'v1_mi6', 'v1_mi6_env2', 'v1_mi5_env2', 'v1_mi6_env2_lstm', 'v1_mi6_env2_mod', 'v1_mi6_env2_lstm_new', 'v1_mi6_hpss', 'v1_mi6_hpss_only', 'v1_mi6_env2_stack', 'v1_mi6_env2_mod_stacks', 'v1_mi6_env2_stacks7', 'v1_mi6_env2_mod_new', 'v1_mi6_mod_stacks7', 'v1_mi6_env2_mod_full_stack', 'context_net', 'v1_mi6_env2_mod_new_stack',  'v1_mi6_env2_mod_new_stack_onset', 'v1_mi6_env2_mod_new_stack8', 'ARNModel_env2_stack', 'ARBModel_stack', 'ARN',
+    'v1', 'v2', 'v3', 'v1_mi6', 'v1_mi6_env2_mod_stacks', 'v1_mi6_env2_mod_new', 'v1_mi6_env2_mod_new_stack', 'v1_mi6_env2_mod_new_stack8', 'ARNModel_env2_stack', 'ARBModel_stack', 'v1_mi6_env2_mod_new_stack8x8',
     'custom1DCNN', 'custom2DCNN', 'LogMelSpectrogramLayer', 'EnvelopeFollowingLayerTorchScript', 'ARB', 'customARB', 'ARB1d',
     'LoadModel', 'ModelSummary', 'ModelTester', 'ModelInit', 'ModelTrainer', 'PrepareModel',
 ]
--- models/utils.py
+++ models/utils.py
@@ -15,8 +15,8 @@ import humanize
 import sys
 import pandas as pd
 
-from models import v1, v2, v3, v2_1d, v1_mi4, v1_mi6_env2_lstm, v1_mi6_env2_mod, v1_mi6_env2_lstm_new, v1_mi6_hpss, v1_mi6_hpss_only, v1_mi6_env2_stack, v1_mi6_env2_mod_stacks, v1_mi6_env2_stacks7, v1_mi6_env2_mod_new
-from models import v1_mi6, v1_mi6_env2, v1_mi5_env2, v1_mi6_mod_stacks7, v1_mi6_env2_mod_full_stack, context_net, v1_mi6_env2_mod_new_stack, v1_mi6_env2_mod_new_stack_onset, v1_mi6_env2_mod_new_stack8, ARNModel_env2_stack, ARBModel_stack, ARN
+from models import v1, v2, v3, v1_mi6_env2_mod_stacks, v1_mi6_env2_mod_new, v1_mi6_env2_mod_new_stack8x8
+from models import v1_mi6, v1_mi6_env2_mod_new_stack, v1_mi6_env2_mod_new_stack8, ARNModel_env2_stack, ARBModel_stack
 
 from tqdm import tqdm
 
@@ -42,29 +42,14 @@ class LoadModel:
             'v1': v1,
             'v2': v2,
             'v3': v3,
-            'v2_1d': v2_1d,
-            'v1_mi4': v1_mi4,
             'v1_mi6': v1_mi6,
-            'v1_mi6_env2': v1_mi6_env2,
-            'v1_mi5_env2': v1_mi5_env2, 
-            'v1_mi6_env2_lstm': v1_mi6_env2_lstm,
-            'v1_mi6_env2_mod': v1_mi6_env2_mod,
-            'v1_mi6_env2_lstm_new': v1_mi6_env2_lstm_new,
-            'v1_mi6_hpss': v1_mi6_hpss,
-            'v1_mi6_hpss_only': v1_mi6_hpss_only,
-            'v1_mi6_env2_stack': v1_mi6_env2_stack,
             'v1_mi6_env2_mod_stacks': v1_mi6_env2_mod_stacks,
-            'v1_mi6_env2_stacks7': v1_mi6_env2_stacks7,
             'v1_mi6_env2_mod_new': v1_mi6_env2_mod_new,
-            'v1_mi6_mod_stacks7': v1_mi6_mod_stacks7,
-            'v1_mi6_env2_mod_full_stack': v1_mi6_env2_mod_full_stack,
-            'context_net': context_net,
             'v1_mi6_env2_mod_new_stack': v1_mi6_env2_mod_new_stack,
-            'v1_mi6_env2_mod_new_stack_onset': v1_mi6_env2_mod_new_stack_onset,
             'v1_mi6_env2_mod_new_stack8': v1_mi6_env2_mod_new_stack8,
             'ARNModel_env2_stack': ARNModel_env2_stack,
             'ARBModel_stack': ARBModel_stack,
-            'ARN': ARN
+            "v1_mi6_env2_mod_new_stack8x8": v1_mi6_env2_mod_new_stack8x8
         }
     
     def get_model(self, model_name, output_nbr, args):
