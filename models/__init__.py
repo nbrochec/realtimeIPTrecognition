d139399277739a4bdea018fb4c0e16b77055407f
nbrc <47108281+nbrochec@users.noreply.github.com>
2024-11-01 15:35:47 +0900

    Remove import of unused models and layers

--- models/__init__.py
+++ models/__init__.py
@@ -1,10 +1,9 @@
-from .models import ismir_A, ismir_B, ismir_C, ismir_D, ismir_E, ismir_F, ismir_G, ismir_Eb
-from .layers import customCNN1D, customCNN2D, LogMelSpectrogramLayer, EnvelopeFollowingLayerTorchScript
+from .models import ismir_Ea, ismir_Eb, ismir_Ec
+from .layers import customCNN2D, LogMelSpectrogramLayer
 from .utils import LoadModel, ModelSummary, ModelTester, ModelInit, ModelTrainer, PrepareModel, TestSavedModel
 
 __all__=[
-    'ismir_A', 'ismir_B', 'ismir_C', 'ismir_D', 'ismir_E', 'ismir_F', 'ismir_G', 'ismir_Eb',
+    'ismir_Ea', 'ismir_Eb', 'ismir_Ec',
     'customCNN1D', 'customCNN2D', 'LogMelSpectrogramLayer', 'EnvelopeFollowingLayerTorchScript',
     'LoadModel', 'ModelSummary', 'ModelTester', 'ModelInit', 'ModelTrainer', 'PrepareModel', 'TestSavedModel'
 ]
-
